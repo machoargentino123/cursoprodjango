651232d87f783baa90807999bb8cbf3beeabf278
machoargentino123 <nahuelirazausta@gmail.com>
2021-01-10 15:07:41 -0300

    borre aplications

--- applications/departamentos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Departamento 
-
-#aca debemos declarar la base de datos para poder administrarla desde el panel # 
-#administrdor de django#
-admin.site.register(Departamento)
